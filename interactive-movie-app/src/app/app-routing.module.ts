5b6876694e2c5130eee213ce03e02b20e210dc94
b_stocker <b.stocker@continuum.tv>
2020-03-18 18:14:44 +0100

    Initial Layout for home page and routing setup

--- interactive-movie-app/src/app/app-routing.module.ts
+++ interactive-movie-app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MovieHomeComponent } from './components/movie/home/movie-home.component';
+import { CreditsComponent } from './components/credits/credits.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'movie', component: MovieHomeComponent},
+  { path: 'credits', component: CreditsComponent},
+  { path: 'impressum', component: ImpressumComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- interactive-movie-app/src/app/app.module.ts
+++ interactive-movie-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
+import { CreditsComponent } from './components/credits/credits.component';
+import { TimelineComponent } from './components/movie/timeline/timeline.component';
+import { MovieHomeComponent } from './components/movie/home/movie-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ImpressumComponent,
+    CreditsComponent,
+    MovieHomeComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
