075f3fc57fa6cc5814f25fa5781936ee942f0b1f
b_stocker <b.stocker@continuum.tv>
2020-03-25 17:35:46 +0100

    Scene plays intro - alt1 - outro now

--- interactive-movie-app/src/app/components/movie/movie-video/movie-video.component.ts
+++ interactive-movie-app/src/app/components/movie/movie-video/movie-video.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { videoSequence } from 'src/app/_models/Interactions';
+import { videoSequence, Interaction } from 'src/app/_models/Interactions';
 
 @Component({
   selector: 'app-movie-video',
@@ -8,6 +8,7 @@ import { videoSequence } from 'src/app/_models/Interactions';
 })
 export class MovieVideoComponent implements OnInit, AfterViewInit {
 
+  @Input() interaction: Interaction;
   @Input() video: videoSequence;
 
   @ViewChild('videoPlayer', {static: false})
@@ -19,7 +20,14 @@ export class MovieVideoComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    console.log(this.videoPlayer.nativeElement);
+  }
+
+  getVideoClass(pos: string) {
+    if (pos === "intro") {
+      return "currentVideo";
+    } else {
+      return "hiddenVideo";
+    }
   }
 
   videoClick(e: Event) {
@@ -27,6 +35,20 @@ export class MovieVideoComponent implements OnInit, AfterViewInit {
       this.videoPlayer.nativeElement.play();
     } else {
       this.videoPlayer.nativeElement.pause();
+    }
+  }
+
+  videoEnded() {
+    let introElement = document.getElementById(this.interaction.sceneId + '-intro');
+    let alt1Element = document.getElementById(this.interaction.sceneId + '-alt-1');
+    let alt2Element = document.getElementById(this.interaction.sceneId + '-alt-2');
+    let outroElement = document.getElementById(this.interaction.sceneId + '-outro');
+    if (this.video.sequencePosition === 'intro') {
+      introElement.classList.replace('currentVideo', 'hiddenVideo');
+      alt1Element.classList.replace('hiddenVideo', 'currentVideo');
+    } else if (this.video.sequencePosition === 'alt-1') {
+      alt1Element.classList.replace('currentVideo', 'hiddenVideo');
+      outroElement.classList.replace('hiddenVideo', 'currentVideo');
     }
   }
 
