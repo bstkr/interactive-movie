78a10c71f8e851cdedfaf7bcbc306482f939e2ef
jan <jan.vogel@uni-ulm.de>
2020-04-15 16:39:39 +0200

    Timeline zeigt nun nur Objekt, welches auch geclicked wurde (Platzhalter Bild)

--- interactive-movie-app/src/app/components/movie/timeline-object/timeline-object.component.ts
+++ interactive-movie-app/src/app/components/movie/timeline-object/timeline-object.component.ts
@@ -20,5 +20,67 @@ export class TimelineObjectComponent implements OnInit {
       .subscribe((s) => (this.objectClicked = s));
   }
 
-  ifUnterhose() {}
+  ifUnterhose() {
+    const unterhose = document.getElementById("a");
+
+    if(this.objectName == "Unterhose" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifBier() {
+    const bier = document.getElementById("b");
+
+    if(this.objectName == "Bier" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifJacke() {
+    const bier = document.getElementById("c");
+
+    if(this.objectName == "Jacke" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifEinkaufszettel() {
+    const bier = document.getElementById("d");
+
+    if(this.objectName == "Einkaufszettel" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifHandy() {
+    const bier = document.getElementById("e");
+
+    if(this.objectName == "Handy" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifComputer() {
+    const bier = document.getElementById("f");
+
+    if(this.objectName == "Computer" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifZeitung() {
+    const bier = document.getElementById("g");
+
+    if(this.objectName == "Zeitung" && this.objectClicked) {
+      return true;
+    }  
+  }
+
+  ifNummer() {
+    const bier = document.getElementById("h");
+
+    if(this.objectName == "Nummer" && this.objectClicked) {
+      return true;
+    }  
+  }
 }
