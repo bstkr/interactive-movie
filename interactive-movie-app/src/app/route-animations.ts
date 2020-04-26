49eb72713a2261bd0d66d3fd3dd1ec6fc77b1842
Tobias V. Aescht <tobias.aescht@uni-ulm.de>
2020-04-26 17:00:59 +0200

    fixes for build

--- interactive-movie-app/src/app/route-animations.ts
+++ interactive-movie-app/src/app/route-animations.ts
@@ -1,76 +1,96 @@
 import {
-    trigger,
-    transition,
-    style,
-    query,
-    group,
-    animateChild,
-    animate,
-    keyframes,
-  } from '@angular/animations';
+  trigger,
+  transition,
+  style,
+  query,
+  group,
+  animateChild,
+  animate,
+  keyframes,
+} from "@angular/animations";
 
+export const slider = trigger("routeAnimations", [
+  transition("* => isLeft", slideTo("left")),
+  transition("isLeft => *", slideTo("right")),
+  transition("isBottom => *", slideToB()),
+  transition("* => isBottom", slideToT()),
+]);
 
-  export const slider =
-  trigger('routeAnimations', [
-    transition('* => isLeft', slideTo('left') ),
-    transition('isLeft => *', slideTo('right') ),
-    transition('isBottom => *', slideToB() ),
-    transition('* => isBottom', slideToT() )
-  ]);
-
-  function slideTo(direction) {
-    const optional = { optional: true };
-    return [
-      query(':enter, :leave', [
+export function slideTo(direction) {
+  const optional = { optional: true };
+  return [
+    query(
+      ":enter, :leave",
+      [
         style({
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           [direction]: 0,
-          width: '100%'
-        })
-      ], optional),
-      query(':enter', [
-        style({ [direction]: '-100%'})
-      ]),
-      group([
-        query(':leave', [
-          animate('1200ms ease', style({ [direction]: '100%'}))
-        ], optional),
-        query(':enter', [
-          animate('1200ms ease', style({ [direction]: '0%'}))
-        ])
-      ]),
-    ];
-  }
+          width: "100%",
+        }),
+      ],
+      optional
+    ),
+    query(":enter", [style({ [direction]: "-100%" })]),
+    group([
+      query(
+        ":leave",
+        [animate("1200ms ease", style({ [direction]: "100%" }))],
+        optional
+      ),
+      query(":enter", [animate("1200ms ease", style({ [direction]: "0%" }))]),
+    ]),
+  ];
+}
 
-  function slideToB() {
-    return [
-      query(':enter, :leave', style({ position: 'fixed', left: 0, right: 0, top: 0, bottom: 0 }), { optional: true }),
-      query(':leave', style({ transform: 'translateY(0%)' }), { optional: true }),
-      query(':enter', style({ transform: 'translateY(-100%)' }), { optional: true }),
-      group([
-        query(':leave', [
-          animate('1200ms ease', style({ transform: 'translateY(100%)' })),
-        ], { optional: true }),
-        query(':enter', [
-          animate('1200ms ease', style({ transform: 'translateY(0%)' })),
-        ], { optional: true })
-      ])
-    ];
-  }
+export function slideToB() {
+  return [
+    query(
+      ":enter, :leave",
+      style({ position: "fixed", left: 0, right: 0, top: 0, bottom: 0 }),
+      { optional: true }
+    ),
+    query(":leave", style({ transform: "translateY(0%)" }), { optional: true }),
+    query(":enter", style({ transform: "translateY(-100%)" }), {
+      optional: true,
+    }),
+    group([
+      query(
+        ":leave",
+        [animate("1200ms ease", style({ transform: "translateY(100%)" }))],
+        { optional: true }
+      ),
+      query(
+        ":enter",
+        [animate("1200ms ease", style({ transform: "translateY(0%)" }))],
+        { optional: true }
+      ),
+    ]),
+  ];
+}
 
-  function slideToT() {
-    return [
-      query(':enter, :leave', style({ position: 'fixed', left: 0, right: 0, top: 0, bottom: 0 }), { optional: true }),
-      query(':leave', style({ transform: 'translateY(0%)' }), { optional: true }),
-      query(':enter', style({ transform: 'translateY(100%)' }), { optional: true }),
-      group([
-        query(':leave', [
-          animate('1200ms ease', style({ transform: 'translateY(-100%)' })),
-        ], { optional: true }),
-        query(':enter', [
-          animate('1200ms ease', style({ transform: 'translateY(0%)' })),
-        ], { optional: true })
-      ])
-    ];
-  }
+export function slideToT() {
+  return [
+    query(
+      ":enter, :leave",
+      style({ position: "fixed", left: 0, right: 0, top: 0, bottom: 0 }),
+      { optional: true }
+    ),
+    query(":leave", style({ transform: "translateY(0%)" }), { optional: true }),
+    query(":enter", style({ transform: "translateY(100%)" }), {
+      optional: true,
+    }),
+    group([
+      query(
+        ":leave",
+        [animate("1200ms ease", style({ transform: "translateY(-100%)" }))],
+        { optional: true }
+      ),
+      query(
+        ":enter",
+        [animate("1200ms ease", style({ transform: "translateY(0%)" }))],
+        { optional: true }
+      ),
+    ]),
+  ];
+}
