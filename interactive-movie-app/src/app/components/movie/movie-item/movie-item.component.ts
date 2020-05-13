232699e0199b7b257339d5082be73ff2d2059666
bstkr <54973055+bstkr@users.noreply.github.com>
2020-05-13 17:07:34 +0200

    #149 rewatch shows blackscreen (#154)
    * fixed blackscreen issue
    
    * #145 fixed
    
    * removed console.log

--- interactive-movie-app/src/app/components/movie/movie-item/movie-item.component.ts
+++ interactive-movie-app/src/app/components/movie/movie-item/movie-item.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input, HostListener } from "@angular/core";
+import { Component, OnInit, Input, HostListener, OnDestroy } from "@angular/core";
 import { InteractionState } from "src/app/_models/InteractionState";
 import { InteractionService } from "src/app/_services/interaction.service";
 import { SceneService } from "src/app/_services/scene.service";
 import { UserState } from "src/app/_models/UserState";
 import { UserService } from "src/app/_services/user.service";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: "app-movie-item",
   templateUrl: "./movie-item.component.html",
   styleUrls: ["./movie-item.component.scss"],
 })
-export class MovieItemComponent implements OnInit {
+export class MovieItemComponent implements OnInit, OnDestroy {
   @Input() interactionState: InteractionState;
 
   interactionClicked: boolean;
@@ -19,21 +20,33 @@ export class MovieItemComponent implements OnInit {
   userState: UserState;
   intorChecker = false;
 
+  subArray: Subscription[];
+
   constructor(
     public interactionService: InteractionService,
     public userService: UserService
   ) {}
 
   ngOnInit() {
+    this.subArray = [];
+
+    this.subArray.push(
     this.interactionService
       .isInteractionClicked(this.interactionState.name)
-      .subscribe((s) => (this.interactionClicked = s));
+      .subscribe((s) => (this.interactionClicked = s)),
     this.interactionService
       .getInteractionState(this.interactionState.name)
       .decision.subscribe((d) => {
         this.interactionDecision = d.split(",")[0];
-      });
+      })
+    );
     this.userService.getUserState().subscribe((s) => (this.userState = s));
+  }
+
+  ngOnDestroy() {
+    for (const sub of this.subArray) {
+      sub.unsubscribe();
+    }
   }
 
   close() {
