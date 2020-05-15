d49f4170e30ed7a30ab6425872d8077b108fedd7
bstkr <benne.stocker@gmail.com>
2020-05-15 09:16:55 +0200

    check if user is in landscape mode

--- interactive-movie-app/src/app/app.component.ts
+++ interactive-movie-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slider } from './route-animations';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,33 @@ import { slider } from './route-animations';
   styleUrls: ['./app.component.scss'],
   animations: [ slider ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'interactive-movie-app';
+
+  landscapeModeSubject: BehaviorSubject<boolean>;
+  landscapeMode: boolean;
+
+  ngOnInit() {
+    this.landscapeModeSubject = new BehaviorSubject(this.checkLandscapeMode());
+
+    window.addEventListener("orientationchange", () => {
+      this.landscapeMode = !this.landscapeMode
+      this.landscapeModeSubject.next(this.landscapeMode);
+    }, false);
+  }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
+
+  checkLandscapeMode(): boolean {
+    console.log(window.innerWidth > window.innerHeight);
+    if (window.innerWidth > window.innerHeight) {
+      this.landscapeMode = true;
+      return true;
+    } else {
+      this.landscapeMode = false;
+      return false;
+    }
   }
 }
